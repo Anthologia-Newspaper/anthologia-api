abd48f3ed3cd98d93c1b064a36d725915e3fb191
Antoine Gavira <antoine.gavira-bottari@epitech.eu>
2025-01-22 18:14:00 +0100

    fix(topic): bug where topic deletion was not cascade

--- src/topics/topics.service.ts
+++ src/topics/topics.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 import { CreateTopicDto } from './dto/create-topic.dto';
 import { UpdateTopicDto } from './dto/update-topic.dto';
+import { IPFSService } from 'src/ipfs/ipfs.service';
+import { ArticlesService } from 'src/articles/articles.service';
 
 @Injectable()
 export class TopicsService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly ipfsService: IPFSService,
+    private readonly articleService: ArticlesService
+  ) {}
 
   async create(newTopic: CreateTopicDto) {
     return await this.prisma.topic.create({ data: newTopic });
@@ -24,6 +30,12 @@ export class TopicsService {
   }
 
   async remove(id: number) {
+    const articles = await this.prisma.article.findMany({ where: { topicId: id } });
+
+    for (const article of articles) {
+      await this.articleService.remove(article.id);
+    }
+
     return await this.prisma.topic.delete({ where: { id } });
   }
 }
